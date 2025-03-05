a2e1d911315b038a288e35c506929cbf76ca65b9
elias provincia <provinciaelias@gmail.com>
2025-03-05 17:26:08 -0500

    add bfunction

--- src/main/java/_Function.java
+++ src/main/java/_Function.java
@@ -1,18 +1,23 @@
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
 
-       int num2=increment.apply(45);
-       int num3=multiplyBy10.apply(45);
+        //Function
+       int num2=increment.apply(1);
+       int num3=multiplyBy10.apply(1);
 
        Function<Integer,Integer> add1AndMultiplyBy10 = increment.andThen(multiplyBy10);
 
-         System.out.println(add1AndMultiplyBy10.apply(1));
+       //bifunction
+       int num4=add.apply(1,100);
+         System.out.println(num4);
     }
     static Function<Integer,Integer> increment = x -> x + 1;
 
+
     static Function<Integer,Integer> multiplyBy10 = x -> x * 10;
 
-
+    static BiFunction<Integer,Integer,Integer> add = (increment,multiplyBy10) -> increment + multiplyBy10;
 }
