5c5cc69c2f7e2e66d30378533c800123a21657ab
elias provincia <provinciaelias@gmail.com>
2025-03-04 23:44:55 -0500

    FEAT: add new function nd aply methos funcion andthen

--- src/main/java/_Function.java
+++ src/main/java/_Function.java
@@ -2,13 +2,17 @@ import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
-       int num= increment(0);
 
        int num2=increment.apply(45);
-         System.out.println(num2);
+       int num3=multiplyBy10.apply(45);
+
+       Function<Integer,Integer> add1AndMultiplyBy10 = increment.andThen(multiplyBy10);
+
+         System.out.println(add1AndMultiplyBy10.apply(1));
     }
     static Function<Integer,Integer> increment = x -> x + 1;
-    static int increment(int number){
-        return number + 1;
-    }
+
+    static Function<Integer,Integer> multiplyBy10 = x -> x * 10;
+
+
 }
